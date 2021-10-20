2ff311b49910de26f78074b329b1feefe0bd79bb
Michelle Jiam <mjiam@student.codam.nl>
2021-10-20 17:34:58 +0200

    Added all class function declarations for vector

--- containers/vector.hpp
+++ containers/vector.hpp
@@ -6,7 +6,7 @@
 /*   By: mjiam <mjiam@student.codam.nl>               +#+                     */
 /*                                                   +#+                      */
 /*   Created: 2021/10/11 17:42:12 by mjiam         #+#    #+#                 */
-/*   Updated: 2021/10/14 20:59:27 by mjiam         ########   odam.nl         */
+/*   Updated: 2021/10/20 16:21:57 by mjiam         ########   odam.nl         */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -17,6 +17,7 @@
 // #include "../utils/iterator_traits.hpp"
 
 namespace   ft {
+
 template < class T, class Alloc = std::allocator<T> >
 class vector {
 	public:
@@ -44,34 +45,136 @@ class vector {
 		vector(InputIterator first, InputIterator last,
 				allocator_type const& alloc = allocator_type());
 		// copy: container with copies of each element in x
-		vector(vector const& x);
+		vector(vector const& other);
 		// assignment operator
-		vector&	operator=(vector const& x);
+		vector&	operator=(vector const& other);
 
 		// DESTRUCTOR
 		~vector(void);
 
 		// ITERATORS
+		iterator				begin(void);
+		const_iterator			begin(void) const;
+		iterator				end(void);
+		const_iterator			end(void) const;
+		reverse_iterator		rbegin(void);
+		const_reverse_iterator	rbegin(void) const;
+		reverse_iterator		rend(void);
+		const_reverse_iterator	rend(void) const;
 
 		// CAPACITY FUNCTIONS
+		// Returns current number of elements.
+		size_type	size(void) const;
+
+		// Returns max number of elements that the vector can hold.
+		size_type	max_size(void) const;
+
+		// Resizes container to contain n elements. If container size > `n`,
+		// container is reduced to `n`. If `n` > size, container is 
+		// expanded to size `n`. If `val` is specified, new elements 
+		// are initialized as copies of `val`.
+		void		resize (size_type n, value_type val = value_type());
+
+		// Returns number of elements container can have before reallocation
+		// is necessary (done automatically by container).
+		// This is not necessarily equal to container size.
+		size_type	capacity(void) const;
+
+		// Returns whether vector is empty (= size is 0).
+		bool		empty(void) const;
+
+		// Requests enough space for at least `n` elements,
+		// only reallocating if capacity is less than `n`.
+		void		reserve (size_type n);
 
 		// ELEMENT ACCESS FUNCTIONS
+		reference		operator[] (size_type n);
+		const_reference	operator[] (size_type n) const;
+		reference		at (size_type n);
+		const_reference	at (size_type n) const;
+		reference		front(void);
+		const_reference	front(void) const;
+		reference		back(void);
+		const_reference	back(void) const;
 
 		// MODIFIERS
+		// Replaces current contents with elements constructed from
+		// elements in range [first,last] in the same order.
+		template <class InputIterator>
+  		void		assign (InputIterator first, InputIterator last);
+		  
+		// Replaces current contents with `n` elements, each initialized
+		// to a copy of `val`.
+		void		assign (size_type n, const value_type& val);
+
+		// Adds new element to end of vector, copying/moving content of val
+		// to it. Increases size by 1. Automatic reallocation is only triggered
+		// if new size surpasses current capacity.
+		void		push_back (const value_type& val);
+
+		// Removes (and destroys) last element, reducing size by 1.
+		void		pop_back(void);
+
+		// Insert single element at specified `position`, increasing size by 1.
+		iterator	insert (iterator position, const value_type& val);
+
+		// Inserts `n` elements iniitalized to a copy of `val`.
+		void		insert (iterator position, size_type n, const value_type& val);
+
+		// Inserts elements in range [first,last] at `position` in same order.
+		template <class InputIterator>
+		void		insert (iterator position, InputIterator first, InputIterator last);
+
+		// Erases (destroys) single element at `position`, reducing size by 1.
+		iterator	erase (iterator position);
+
+		// Erases a range of elements [first,last].
+		iterator	erase (iterator first, iterator last);
+
+		// Swaps content of container by content of `x` (same type).
+		// Sizes may differ. All iterators, references, pointers remain valid.
+		void		swap (vector& x);
+
+		// Removes (destroys) all elements from vector, leaving size of 0.
+		// Reallocation is not guaranteed and capacity is not guaranteed
+		// to change.
+		// Typical alternative that forces reallocation:
+		//		vector<T>().swap(x);
+		void		clear(void);
 
 		// ALLOCATOR
+		// Returns a copy of the allocator object associated with vector.
+		allocator_type get_allocator() const;
 
 	private:
 		allocator_type	_alloc;
-		pointer			_array;
 		size_type		_size;
 		size_type		_capacity;
+		pointer			_array;
 };
 
 // NON-MEMBER FUNCTION OVERLOADS
+template <class T, class Alloc>
+bool operator== (const vector<T,Alloc>& lhs, const vector<T,Alloc>& rhs);
+template <class T, class Alloc>
+bool operator!= (const vector<T,Alloc>& lhs, const vector<T,Alloc>& rhs);
+template <class T, class Alloc>
+bool operator<  (const vector<T,Alloc>& lhs, const vector<T,Alloc>& rhs);
+template <class T, class Alloc>
+bool operator<= (const vector<T,Alloc>& lhs, const vector<T,Alloc>& rhs);
+template <class T, class Alloc>
+bool operator>  (const vector<T,Alloc>& lhs, const vector<T,Alloc>& rhs);
+template <class T, class Alloc>
+bool operator>= (const vector<T,Alloc>& lhs, const vector<T,Alloc>& rhs);
 
+// Overload of member function `swap` hat improves its performance by
+// mutually transferring ownership over their assets to the other container
+// (i.e., the containers exchange references to their data, without actually
+// performing any element copy or movement).
+template <class T, class Alloc>
+void swap (vector<T,Alloc>& x, vector<T,Alloc>& y);
 
-}
+} // ft namespace
 
 #include "vector.ipp"
 
--- containers/vector.ipp
+++ containers/vector.ipp
@@ -6,41 +6,52 @@
 /*   By: mjiam <mjiam@student.codam.nl>               +#+                     */
 /*                                                   +#+                      */
 /*   Created: 2021/10/12 19:09:49 by mjiam         #+#    #+#                 */
-/*   Updated: 2021/10/14 21:00:38 by mjiam         ########   odam.nl         */
+/*   Updated: 2021/10/20 16:11:35 by mjiam         ########   odam.nl         */
 /*                                                                            */
 /* ************************************************************************** */
 
-// default constructor
+// DEFAULT CONSTRUCTOR
 template <class T, class Alloc>
 ft::vector<T,Alloc>::vector(allocator_type const& alloc)
-		: _alloc(alloc), _array(NULL), _size(0), _capacity(0) {
+		: _alloc(alloc), _size(0), _capacity(0), _array(NULL) {
 }
 
-// fill constructor
+// FILL CONSTRUCTOR
+// Exceptions:
+// allocator::allocate may throw bad alloc;
+// allocator::construct calls value_type's copy constructor, which
+// 	may throw bad_alloc if value_type is class like map/vector/string.
 template <class T, class Alloc>
-ft::vector<T,Alloc>::vector(size_type n, value_type const& val,
+ft::vector<T,Alloc>::vector(size_type count, value_type const& val,
 							allocator_type const& alloc)
-		: _alloc(alloc), _array(alloc.allocate(n)), _size(n), _capacity(n) {
+		:	_alloc(alloc),
+			_size(count),
+			_capacity(count),
+			_array(alloc.allocate(count)) {
 	try {
-		for (size_type i = 0; i < n; i++) {
+		for (size_type i = 0; i < count; i++) {
 			_alloc.construct(&_array[i], val);
 		}
 	}
 	catch (...) {
-		_alloc.deallocate(_array, n);
+		_alloc.deallocate(_array, count);
 		throw;
-	}
+	} // TODO: replace with assign
 }
 
-// range constructor
+// RANGE CONSTRUCTOR
+// Exceptions:
+// std::distance may throw if arithmetical operations performed on the 
+// 	iterators throw;
+// allocator::allocate & allocator:construct may throw bad_alloc.
 template <class T, class Alloc>
 template <class InputIterator>
 ft::vector<T,Alloc>::vector(InputIterator first, InputIterator last,
 							allocator_type const& alloc)
 		:	_alloc(alloc),
 			_size = std::distance(first, last),
-			_array = alloc.allocate(_size),
-			_capacity = _size {
+			_capacity = _size, // TODO: 
+			_array = alloc.allocate(_size) {
 	try {
 		for (size_type i = 0; i < _size; i++) {
 			_alloc.construct(&_array[i], *(first + i));
@@ -49,5 +60,85 @@ ft::vector<T,Alloc>::vector(InputIterator first, InputIterator last,
 	catch (...) {
 		_alloc.deallocate(_array, _size);
 		throw;
+	} // TODO: replace with assign
+}
+
+// COPY CONSTRUCTOR
+template <class T, class Alloc>
+ft::vector<T,Alloc>::vector(vector const& other)
+		: _alloc(other._alloc)  {
+	// *this = other;
+	// copyRange
+	_size = other._size;
+	_capacity = other._capacity;
+	_array = _alloc.allocate(other._capacity);
+	try {
+		for (size_type i = 0; i < _size; i++)
+			_alloc.construct(&_array[i], other._array[i]);
 	}
+	catch (...) {
+		_alloc.deallocate(_array, _size);
+		throw;
+	} // TODO: replace with assign
+}
+
+// ASSIGNMENT OPERATOR
+template <class T, class Alloc>
+vector&	ft::vector<T,Alloc>::operator=(vector const& other) {
+	if (this != &other) {
+		// assign (which both clears and constructs)
+	}
+	return *this;
+}
+
+// DESTRUCTOR
+template <class T, class Alloc>
+ft::vector<T,Alloc>::~vector(void) {
+	// clear
+	_alloc.deallocate(_array, _capacity);
+}
+
+template <class T, class Alloc>
+size_type	ft::vector<T,Alloc>::size(void) const {
+
+}
+
+template <class T, class Alloc>
+size_type	ft::vector<T,Alloc>::max_size(void) const {
+
+}
+
+// Changes only the number of elements in the container, not its capacity.
+template <class T, class Alloc>
+void		ft::vector<T,Alloc>::resize (size_type n, value_type val) {
+
+}
+
+template <class T, class Alloc>
+size_type	ft::vector<T,Alloc>::capacity(void) const {
+
+}
+
+template <class T, class Alloc>
+bool		ft::vector<T,Alloc>::empty(void) const {
+
+}
+
+// Because vectors use an array as their underlying storage, erasing elements
+// in positions other than the vector end causes the container to relocate 
+// all the elements after the segment erased to their new positions.
+// No-throw guarantee if removed elements include last element.
+template <class T, class Alloc>
+iterator	ft::vector<T,Alloc>::erase (iterator position) {
+
+}
+
+// Only reallocates storage, increasing capacity to n or greater,
+// 	if `n` > current capacity. Has no effect on current size.
+// Exceptions:
+// If size requested > vector::max_size, throws length_error exception;
+// If reallocating, allocator may throw bad_alloc.
+template <class T, class Alloc>
+void		ft::vector<T,Alloc>::reserve (size_type n) {
+
 }
