31cfa67453f80f83413480c944c4d271b3a3fb04
Michelle Jiam <mjiam@student.codam.nl>
2022-02-10 21:49:55 +0100

    Empty tree construction working

--- test2/main.cpp
+++ test2/main.cpp
@@ -6,11 +6,13 @@
 /*   By: mjiam <mjiam@student.codam.nl>               +#+                     */
 /*                                                   +#+                      */
 /*   Created: 2022/01/20 16:45:04 by mjiam         #+#    #+#                 */
-/*   Updated: 2022/02/03 21:20:01 by mjiam         ########   odam.nl         */
+/*   Updated: 2022/02/10 21:09:40 by mjiam         ########   odam.nl         */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "tester.hpp"
+
+#include "../utils/rb_tree.hpp" // TODO: remove
 
 namespace std {
     inline std::string get_name() { return "std"; }
@@ -28,5 +30,9 @@ int main() {
     test_vector();
     test_stack();
     
+    ft::rb_tree<int,int> RB;
+    RB.debug_empty();
+    
+    system("leaks ft_bin");
     return 0;
 }
--- utils/rb_tree.hpp
+++ utils/rb_tree.hpp
@@ -6,7 +6,7 @@
 /*   By: mjiam <mjiam@student.codam.nl>               +#+                     */
 /*                                                   +#+                      */
 /*   Created: 2022/02/08 16:47:52 by mjiam         #+#    #+#                 */
-/*   Updated: 2022/02/10 19:11:55 by mjiam         ########   odam.nl         */
+/*   Updated: 2022/02/10 21:47:33 by mjiam         ########   odam.nl         */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -32,28 +32,42 @@ struct	rb_node_base {
 	node_ptr	left;
 	node_ptr	right;
 
-	rb_node_base();
+	// default constructor
+	rb_node_base() : colour(RED), parent(NULL), left(NULL), right(NULL) {}
 	
-	static node_ptr	min_node(node_ptr x) {
-		while (x->left != NULL)
+	// constructor with colour given
+	rb_node_base(rb_colour col)
+		: colour(col), parent(NULL), left(NULL), right(NULL) {}
+
+	// copy constructor
+	rb_node_base(rb_node_base& other)
+		:	colour(other.colour),
+			parent(other.parent),
+			left(other.left),
+			right(other.right) {}
+
+	~rb_node_base() {}
+	
+	static node_ptr	minimum(node_ptr nil, node_ptr x) {
+		while (x->left != nil)
 			x = x->left;
 		return x;
 	}
 
-	static const_node_ptr	min_node(const_node_ptr x) {
-		while (x->left != NULL)
+	static const_node_ptr	minimum(const_node_ptr nil, const_node_ptr x) {
+		while (x->left != nil)
 			x = x->left;
 		return x;
 	}
 
-	static node_ptr	max_node(node_ptr x) {
-		while (x->right != NULL)
+	static node_ptr	maximum(node_ptr nil, node_ptr x) {
+		while (x->right != nil)
 			x = x->right;
 		return x;
 	}
 
-	static const_node_ptr	max_node(const_node_ptr x) {
-		while (x->right != NULL)
+	static const_node_ptr	maximum(const_node_ptr nil, const_node_ptr x) {
+		while (x->right != nil)
 			x = x->right;
 		return x;
 	}
@@ -64,24 +78,25 @@ template <class Val>
 struct	rb_node : rb_node_base {
 	Val	value_type; // Val is a pair<Key,T> object
 
+	// default constructor
+	rb_node() : rb_node_base() {}
+	
+	// constructor with val
 	rb_node(Val val)
-		: colour(RED), parent(NULL), left(NULL), right(NULL), value_type(val) {}
+		: rb_node_base(RED), value_type(val) {}
 
-	rb_node(rb_node& other)
-		:	colour(other.colour),
-			parent(other.parent),
-			left(other.left),
-			right(other.right),
-			value_type(other.val) {}
+	// copy constructor
+	rb_node(rb_node& other) : rb_node_base(other), value_type(other.val) {}
 
 	~rb_node() {}
-}
+};
 
 // iterators
 
 // uses a single sentinel to represent NULL to simplify boundary conditions
-
-template <class Key, class Val, class Compare, class Alloc>
+#include <functional> // TODO: remove
+template <class Key, class Val, class Compare  = std::less<Key>, // TODO: remove default for compare
+			class Alloc = std::allocator<Val> >
 class rb_tree {
 	public:
 		typedef Key					key_type;
@@ -92,36 +107,96 @@ class rb_tree {
 		typedef const value_type&	const_reference;
 		typedef std::size_t			size_type;
 		typedef std::ptrdiff_t		difference_type;
-		typedef Alloc				allocator_type;
 
 	protected:
-		// typedef rb_node<Val>		node_type;
-		// typedef const rb_node<Val>	const_node_type;
-		// typedef node_type*				node_ptr;
-		// typedef const node_type*		const_node_ptr;
-		typedef rb_node*			node_ptr;
-		typedef rb_node_base*		base_ptr;
-		typedef const rb_node*		const_node_ptr;
-
-		base_ptr	nil; // sentinel representing NULL
-		node_ptr	root; // root node
-		size_type	size;
+		typedef rb_node<Val>*			node_ptr;
+		typedef rb_node_base*			base_ptr;
+		typedef const rb_node<Val>*		const_node_ptr;
 	
 	private:
 		typedef typename Alloc::template rebind<rb_node_base>::other
 				_base_type_alloc;
-		typedef typename Alloc::template rebind<rb_node>::other
+		typedef typename Alloc::template rebind<rb_node<Val> >::other
 				_node_type_alloc;
 		
-		_base_type_alloc	sentinel_alloc;
-		_node_type_alloc	node_alloc;
-
-		
-		
-	rb_tree() {
-
-	}
+		_base_type_alloc	_b_alloc;
+		_node_type_alloc	_n_alloc;
 	
+	public:
+	// Constructors & Destructor
+		rb_tree() : _root(NULL), _size(0), _key_compare() {
+			_nil = _create_base();
+			// _root = _create_node(NULL);
+		}
+
+		rb_tree(Compare const& comp)
+			: _root(NULL), _size(0), _key_compare(comp) {
+			_nil = _create_base();
+		}
+
+		rb_tree(rb_tree const& other) {
+			_nil = _create_base();
+			*this = other;
+		}
+
+		~rb_tree() {
+			_erase(_nil, _b_alloc);
+		}
+
+	// Accessors
+		Compare key_comp() const {
+			return _key_compare;
+		}
+
+		bool	empty() const {
+			return _size == 0;
+		}
+
+		size_type	size() const {
+			return _size;
+		}
+
+		size_type	max_size() const {
+			return _n_alloc.max_size();
+		}
+
+		// TODO: remove
+		void	debug_empty() {
+			std::cout << "nil colour is: " << (_nil->colour == RED ? "red" : "black") << std::endl;
+			std::cout << "size is: " << size() << std::endl;
+			std::cout << "empty? " << std::boolalpha << empty() << std::endl;
+		}
+
+	private:
+		base_ptr	_nil; // sentinel representing NULL
+		node_ptr	_root; // root node
+		size_type	_size;
+		Compare		_key_compare;
+		
+		base_ptr	_create_base() {
+			base_ptr tmp = _b_alloc.allocate(1);
+			_b_alloc.construct(tmp, BLACK);
+			return tmp;
+		}
+
+		node_ptr	_create_node(value_type const& val) {
+			node_ptr tmp = _n_alloc.allocate(1);
+			_n_alloc.construct(tmp, rb_node<Val>(val));
+			// tmp->parent = _nil; // TODO: inefficient? see if base constructor can be changed to do this
+			// _set_ends_to_nil(tmp);
+			return tmp;
+		}
+
+		template <class T, class R_Alloc>
+		void	_erase(T& node_type, R_Alloc& alloc) {
+			alloc.destroy(node_type);
+			alloc.deallocate(node_type, 1);
+		}
+
+		void	_set_ends_to_nil(node_ptr node) {
+			node->left = _nil;
+			node->right = _nil;
+		}
 };
 }
 
